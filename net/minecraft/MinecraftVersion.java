656222542424b479372e7a72c6e1ae0c17f0a1a6
Mojang <dev@mojang.com>
2021-11-26 10:02:04 +0000

    1.18-rc2

--- net/minecraft/MinecraftVersion.java
+++ net/minecraft/MinecraftVersion.java
@@ -35,9 +35,9 @@ implements GameVersion {
 
     private MinecraftVersion() {
         this.id = UUID.randomUUID().toString().replaceAll("-", "");
-        this.name = "1.18-rc1";
+        this.name = "1.18-rc2";
         this.stable = false;
-        this.saveVersion = new SaveVersion(2856, "main");
+        this.saveVersion = new SaveVersion(2857, "main");
         this.protocolVersion = SharedConstants.getProtocolVersion();
         this.resourcePackVersion = 8;
         this.dataPackVersion = 8;
