074eb58077716177a3c97e1e41499ea562e4336e
Mojang <dev@mojang.com>
2021-11-30 09:16:29 +0000

    1.18

--- minecraft/src/net/minecraft/MinecraftVersion.java
+++ minecraft/src/net/minecraft/MinecraftVersion.java
@@ -28,9 +28,9 @@ public class MinecraftVersion implements GameVersion {
 
 	private MinecraftVersion() {
 		this.id = UUID.randomUUID().toString().replaceAll("-", "");
-		this.name = "1.18-rc4";
-		this.stable = false;
-		this.saveVersion = new SaveVersion(2859, "main");
+		this.name = "1.18";
+		this.stable = true;
+		this.saveVersion = new SaveVersion(2860, "main");
 		this.protocolVersion = SharedConstants.getProtocolVersion();
 		this.resourcePackVersion = 8;
 		this.dataPackVersion = 8;
--- minecraft/src/net/minecraft/SharedConstants.java
+++ minecraft/src/net/minecraft/SharedConstants.java
@@ -10,13 +10,13 @@ import net.minecraft.util.math.ChunkPos;
 
 public class SharedConstants {
 	@Deprecated
-	public static final boolean IS_DEVELOPMENT_VERSION = true;
+	public static final boolean IS_DEVELOPMENT_VERSION = false;
 	@Deprecated
-	public static final int WORLD_VERSION = 2859;
+	public static final int WORLD_VERSION = 2860;
 	@Deprecated
 	public static final String CURRENT_SERIES = "main";
 	@Deprecated
-	public static final String VERSION_NAME = "1.18-rc4";
+	public static final String VERSION_NAME = "1.18";
 	@Deprecated
 	public static final String RELEASE_TARGET = "1.18";
 	@Deprecated
@@ -173,7 +173,7 @@ public class SharedConstants {
 	}
 
 	public static int getProtocolVersion() {
-		return 1073741884;
+		return 757;
 	}
 
 	public static boolean method_37896(ChunkPos chunkPos) {
