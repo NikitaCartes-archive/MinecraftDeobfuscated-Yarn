6f3c8b43d473c7668693065463444bb4cfc52308
Mojang <dev@mojang.com>
2022-06-03 11:47:25 +0000

    1.19-rc2

--- minecraft/src/net/minecraft/SharedConstants.java
+++ minecraft/src/net/minecraft/SharedConstants.java
@@ -13,17 +13,17 @@ public class SharedConstants {
 	@Deprecated
 	public static final boolean IS_DEVELOPMENT_VERSION = true;
 	@Deprecated
-	public static final int WORLD_VERSION = 3103;
+	public static final int WORLD_VERSION = 3104;
 	@Deprecated
 	public static final String CURRENT_SERIES = "main";
 	@Deprecated
-	public static final String VERSION_NAME = "1.19-rc1";
+	public static final String VERSION_NAME = "1.19-rc2";
 	@Deprecated
 	public static final String RELEASE_TARGET = "1.19";
 	@Deprecated
 	public static final int RELEASE_TARGET_PROTOCOL_VERSION = 759;
 	@Deprecated
-	public static final int field_29736 = 90;
+	public static final int field_29736 = 91;
 	public static final int SNBT_TOO_OLD_THRESHOLD = 3075;
 	private static final int field_29708 = 30;
 	public static final boolean field_36325 = false;
@@ -177,7 +177,7 @@ public class SharedConstants {
 	}
 
 	public static int getProtocolVersion() {
-		return 1073741914;
+		return 1073741915;
 	}
 
 	public static boolean method_37896(ChunkPos chunkPos) {
